ca89f14d2b3c55d941687c61d9b737b800b053c6
PabloML <pablo_martin_lencina@hotmail.com>
2022-07-08 15:08:30 -0300

    Corrección del Doctest

--- Simple/Simple.py
+++ Simple/Simple.py
@@ -8,43 +8,43 @@ def listaDiccionario():
        True
        >>> l[0]['id']==0
        True
-       >>> 0<=l[0]['edad']<=100
+       >>> 1<=l[0]['edad']<=100
        True
        >>> l[1]['id']==1
        True
-       >>> 0<=l[1]['edad']<=100
+       >>> 1<=l[1]['edad']<=100
        True
        >>> l[2]['id']==2
        True
-       >>> 0<=l[2]['edad']<=100
+       >>> 1<=l[2]['edad']<=100
        True
        >>> l[3]['id']==3
        True
-       >>> 0<=l[3]['edad']<=100
+       >>> 1<=l[3]['edad']<=100
        True
        >>> l[4]['id']==4
        True
-       >>> 0<=l[4]['edad']<=100
+       >>> 1<=l[4]['edad']<=100
        True
        >>> l[5]['id']==5
        True
-       >>> 0<=l[5]['edad']<=100
+       >>> 1<=l[5]['edad']<=100
        True
        >>> l[6]['id']==6
        True
-       >>> 0<=l[6]['edad']<=100
+       >>> 1<=l[6]['edad']<=100
        True
        >>> l[7]['id']==7
        True
-       >>> 0<=l[7]['edad']<=100
+       >>> 1<=l[7]['edad']<=100
        True
        >>> l[8]['id']==8
        True
-       >>> 0<=l[8]['edad']<=100
+       >>> 1<=l[8]['edad']<=100
        True
        >>> l[9]['id']==9
        True
-       >>> 0<=l[9]['edad']<=100
+       >>> 1<=l[9]['edad']<=100
        True
     """
     lista=[]
